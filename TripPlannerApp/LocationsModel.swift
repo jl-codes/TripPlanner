93a2d83b36e4746a0d568825c95907893f4c3ab3
Johnny Loehr <johnnyloehr@Johnnys-MacBook-Pro.local>
2019-09-23 12:15:33 -0400

    init

---
+++ TripPlannerApp/LocationsModel.swift
@@ -0,0 +1,9 @@
+//
+//  LocationsModel.swift
+//  TripPlannerApp
+//
+//  Created by Johnny Loehr on 9/22/19.
+//  Copyright © 2019 Johnny Loehr. All rights reserved.
+//
+
+import Foundation
---
+++ TripPlannerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripPlannerApp
+//
+//  Created by Johnny Loehr on 9/22/19.
+//  Copyright © 2019 Johnny Loehr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
