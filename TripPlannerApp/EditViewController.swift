ad02b33dc53fbf19f3d53d971970ce49864b77e0
Johnny Loehr <johnnyloehr@Johnnys-MacBook-Pro.local>
2019-09-24 11:09:52 -0400

    completed ui

--- TripPlannerApp/EditViewController.swift
+++ TripPlannerApp/EditViewController.swift
@@ -10,18 +10,37 @@ import UIKit
 
 class EditViewController: UIViewController {
   var name: String = ""
+  var delegate: EditViewControllerDelegate?
    
   @IBOutlet weak var newStop: UITextField!
+    
+  @IBAction func done(segue:UIStoryboardSegue) {
+    print("done segue started")
+//    let destination = segue.destination as! ViewController
+//    destination.places.append(name)
+    if let newPlace = newStop.text {
+      delegate?.add(newPlace: newPlace)
+    }
+    navigationController?.popViewController(animated: true)
+    dismiss(animated: true, completion: nil)
+  }
     
   override func viewDidLoad() {
     super.viewDidLoad()
 
   }
     
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "doneWithSegue" {
-        name = newStop.text!
-    }
-  }
+//  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    if let destination = segue.destination as? EditViewController {
+//        destination.delegate = self
+//    }
+//    if segue.identifier == "doneWithSegue" {
+//        name = newStop.text!
+//    }
+//  }
     
+}
+
+protocol EditViewControllerDelegate {
+  func add(newPlace: String)
 }
--- TripPlannerApp/ViewController.swift
+++ TripPlannerApp/ViewController.swift
@@ -9,48 +9,65 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UITableViewController {
-    
+class ViewController: UIViewController {
+  
   var places = [String]()
   var newPlace: String = ""
-//
-//  @IBOutlet weak var table: UITableView!
-    
-//  @IBAction func cancel(segue:UIStoryboardSegue) {
-//    
-//  }
-    
-  @IBAction func done(segue:UIStoryboardSegue) {
-    print("done segue started")
-    let newPlaceViewController = segue.source as! EditViewController
-    newPlace = newPlaceViewController.name
-    places.append(newPlace)
-    tableView.reloadData()
-  }
-    
+  
+  @IBOutlet weak var table: UITableView!
+  
+  //  @IBAction func cancel(segue:UIStoryboardSegue) {
+  //
+  //  }
+  
+  //  @IBAction func done(segue:UIStoryboardSegue) {
+  //    print("done segue started")
+  //    let newPlaceViewController = segue.source as! EditViewController
+  //    newPlace = newPlaceViewController.name
+  //    places.append(newPlace)
+  //    tableView.reloadData()
+  //  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-//    tableView.dataSource = self
-//    tableView.delegate = self
-//    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "placeCell")
- 
+    //    tableView.dataSource = self
+    //    tableView.delegate = self
+    //    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "placeCell")
+    
     places = ["10 Rimington Ln, Decatur", "2525 N Decatur Rd, Decatur", "3393 Peachtree Rd NE, Atlanta", "816 N Highland Ave NE, Atlanta"]
   }
-    
-  override func numberOfSections(in table: UITableView) -> Int {
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let destination = segue.destination as? EditViewController {
+      destination.delegate = self
+    }
+  }
+  
+  
+  
+}
+
+extension ViewController: UITableViewDataSource {
+  func numberOfSections(in table: UITableView) -> Int {
     return 1
   }
-
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return places.count
   }
-
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell", for: indexPath)
-
+    
     cell.textLabel?.text = places[indexPath.row]
-
+    
     return cell
   }
+}
 
+extension ViewController: EditViewControllerDelegate {
+  func add(newPlace: String) {
+    places.append(newPlace)
+    table.reloadData()
+  }
 }
